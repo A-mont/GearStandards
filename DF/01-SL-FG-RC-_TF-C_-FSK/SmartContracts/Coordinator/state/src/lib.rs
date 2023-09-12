7fd84c750cc0d3889a58453f0238991cc1238722
A Mon <a.mont.24.777@gmail.com>
2023-09-12 17:25:36 -0600

    Update

--- DF/01-SL-FG-RC-_TF-C_-FSK/SmartContracts/Coordinator/state/src/lib.rs
+++ DF/01-SL-FG-RC-_TF-C_-FSK/SmartContracts/Coordinator/state/src/lib.rs
@@ -13,21 +13,5 @@ include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 pub mod metafns {
     pub type State = <ContractMetadata as Metadata>::State;
 
-    /// Get a list of pingers.
-    pub fn pingers(state: State) -> Vec<ActorId> {
-        state.into_iter().map(|(pinger, _)| pinger).collect()
-    }
-
-    /// Get ping count for a pinger.
-    pub fn ping_count(state: State, actor: ActorId) -> u128 {
-        state
-            .into_iter()
-            .find_map(|(pinger, ping_count)| (pinger == actor).then_some(ping_count))
-            .unwrap_or_default()
-    }
-
-    
-     pub fn finalstates(state: State, actor: ActorId) -> Vec<&Str> {
-        state.into_iter().map(|(_, state)| state).collect()
-    }
+   // Add your State functions
 }
